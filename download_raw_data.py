04e393f3f3d7d449af6aac9eb9267bc0ca61a2f6
dklink <dougdklink3@gmail.com>
2023-06-24 14:01:13 -0600

    wrote script to download some raw data from CAISO, and extract it into a usable csv form

---
+++ download_raw_data.py
@@ -0,0 +1,27 @@
+"""
+download some daily electricity output data from california
+
+"""
+from datetime import datetime, timedelta
+from urllib.error import HTTPError
+from tqdm import tqdm
+import wget
+from pathlib import Path
+
+start_date = datetime(year=2018, month=6, day=1)
+end_date = datetime(year=2021, month=5, day=31)
+
+out_path = Path("CA_daily_renewables")
+skipped_dates = []
+date = start_date
+with tqdm(total=(end_date-start_date).days) as pbar:
+    while date <= end_date:
+        url = f"https://content.caiso.com/green/renewrpt/{date:%Y%m%d}_DailyRenewablesWatch.txt"
+        try:
+            wget.download(url, out=str(out_path))
+        except HTTPError:
+            print(f"Download failed for date {date:%Y%m%d}.")
+        date += timedelta(days=1)
+        pbar.update()
+
+print("Complete.")
---
+++ extract_hourly_data.py
@@ -0,0 +1,26 @@
+"""
+from the raw downloaded files, extract hourly data for the different types of renewables.
+"""
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
+
+raw_dir = Path("CA_daily_renewables")
+
+daily_data = []
+for file in sorted(raw_dir.glob("*.txt")):
+    try:
+        hourly_data = pd.read_csv(
+            file,
+            delim_whitespace=True, skiprows=2, nrows=24,
+            names=["HOUR", "GEOTHERMAL", "BIOMASS", "BIOGAS", "SMALL HYDRO", "WIND TOTAL", "SOLAR PV", "SOLAR THERMAL"],
+        )
+        date = datetime.strptime(file.name.split("_")[0], "%Y%m%d")
+        hourly_data["HOUR"] = [date + timedelta(hours=hour-1) for hour in hourly_data["HOUR"]]
+        hourly_data = hourly_data.rename(columns={"HOUR":"DATETIME"})
+        daily_data.append(hourly_data)
+    except pd.errors.ParserError:  # malformed file caused by insufficient data to calculate hourly information
+        print(f"Couldn't parse file {file}")
+    
+all_data = pd.concat(daily_data, ignore_index=True)
+all_data.to_csv("CA_daily_renawables.csv")
